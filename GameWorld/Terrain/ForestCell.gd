46b486f6eb74708edf3b474f917fa04446a59a78
Piotr Hajduga <piotr.hajduga@gmail.com>
2018-03-09 22:21:22 +0100

    Trees continued

--- GameWorld/Terrain/ForestCell.gd
+++ GameWorld/Terrain/ForestCell.gd
@@ -1,8 +1,15 @@
 extends Spatial
 
-export(NodePath) var world_data_node
 var world_data
+onready var game_space = get_node("/root/GameSpace")
+onready var Tree = preload("res://GameWorld/Terrain/Tree.tscn")
+
+export(int) var radius = 1
 
 func _ready():
-	for tree in self.get_children():
-		tree.translation.y = world_data.get_terrain_mesh_height(to_global(tree.translation))
+	for offset in game_space.offset_range(Vector2(0,0),radius):
+		var pos = game_space.offset_to_world(offset) / (2*radius+1)
+		var tree = Tree.instance()
+		tree.translation = pos
+		tree.translation.y = world_data.get_terrain_mesh_height(to_global(pos))
+		add_child(tree)
